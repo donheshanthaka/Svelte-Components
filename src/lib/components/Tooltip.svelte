9927b99436e6a3eb92320f12d04bad10a82d2692
donheshanthaka <heshanthaka007@gmail.com>
2023-06-01 23:40:43 +0530

    Update tooltip component

--- src/lib/components/Tooltip.svelte
+++ src/lib/components/Tooltip.svelte
@@ -2,35 +2,32 @@
     export let toolTipData;
 </script>
 
-
 <div>
-    <a href="#" class="mt-10 mx-10 group relative inline-block text-blue-500 underline hover:text-red-500 duration-300">
-        Link with right tooltip
-        <span class="tooltipRight {toolTipData.backgroundColor} {toolTipData.textColor}">This
-            {toolTipData.text}
-        </span>
-    </a>
-</div>
-
-
-<div class="mt-10 mx-10">
-    <a href="#" class="group relative inline-block text-blue-500 underline hover:text-red-500 duration-300">
-        Link with top tooltip
-        <!-- Tooltip text here -->
-        <span class="tooltipTop {toolTipData.backgroundColor} {toolTipData.textColor}" >This
-            {toolTipData.text}
+    <a href="#" class="group">
+        {toolTipData.linkText}
+        <span class="{toolTipData.direction} {toolTipData.backgroundColor} {toolTipData.textColor}">
+            {toolTipData.toolTipText}
         </span>
     </a>
 </div>
 
 
 <style>
+
+    a {
+        @apply relative inline-block text-blue-500 underline hover:text-red-500 duration-300
+    }
+
     .tooltipTop {
-        @apply absolute hidden group-hover:flex -left-5 -top-2 -translate-y-full w-48 px-2 py-1 rounded-lg text-center text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700;
+        @apply absolute hidden group-hover:flex -left-5 -top-2 -translate-y-full px-2 py-1 rounded-lg text-center
+        text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8
+        after:border-x-transparent after:border-b-transparent after:border-t-gray-700 translate-x-5;
     }
 
     .tooltipRight {
-        @apply absolute hidden group-hover:flex -top-2 -right-3 translate-x-full w-48 px-2 py-1 rounded-lg text-center text-sm before:content-[''] before:absolute before:top-1/2  before:right-[100%] before:-translate-y-1/2 before:border-8 before:border-y-transparent before:border-l-transparent before:border-r-gray-700;
+        @apply absolute hidden group-hover:flex -top-1 -right-3 translate-x-full px-2 py-1 rounded-lg text-center
+        text-sm before:content-[''] before:absolute before:top-1/2  before:right-[100%] before:-translate-y-1/2
+        before:border-8 before:border-y-transparent before:border-l-transparent before:border-r-gray-700;
     }
 
 </style>
--- src/routes/tooltip/+page.svelte
+++ src/routes/tooltip/+page.svelte
@@ -1,21 +1,26 @@
 <script>
     import Tooltip from "$lib/components/Tooltip.svelte";
 
-    const toolTipData = {
-        text: 'This is the tool tip text',
+    const topToolTipData = {
+        linkText: 'Link with top Tooltip',
+        toolTipText: 'This is the tool tip text',
+        textColor: 'text-white',
+        backgroundColor: 'bg-indigo-700',
+        direction: 'tooltipTop',
+    }
+    const rightToolTipData = {
+        linkText: 'Link with right Tooltip',
+        toolTipText: 'This is the tool tip text',
         textColor: 'text-white',
         backgroundColor: 'bg-gray-700',
-        duration: ''
+        direction: 'tooltipRight',
     }
 </script>
 
 
-<Tooltip {toolTipData}/>
+<div class="mt-10 mx-10 flex flex-col gap-12">
+    <Tooltip toolTipData={topToolTipData}></Tooltip>
+    <Tooltip toolTipData={rightToolTipData}></Tooltip>
+</div>
 
 
-<button class="mx-10 my-10 flex items-center justify-center px-4 py-2 font-medium text-white bg-indigo-500 transition duration-150 ease-in-out hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 dark:bg-primary-400 dark:hover:bg-primary-500 dark:focus:bg-primary-500 dark:active:bg-primary-600 rounded-lg shadow"
-        data-te-toggle="tooltip"
-        title={toolTipData.text}>
-    <span class="mr-1">Button</span>
-    <span class="text-xs opacity-75">(Hover for tooltip)</span>
-</button>
